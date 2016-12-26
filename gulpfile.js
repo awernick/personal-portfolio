09c1c5bca3d3554c2b135a6e5f9b1ec092919dd6
Alan <alan.wernik@gmail.com>
2016-12-25 21:37:59 -0700

    Finish styling for footer

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,29 @@
-var gulp        = require('gulp'),
-    del         = require('del'),
-    sass        = require('gulp-sass'),
-    concat      = require('gulp-concat'),
-    uglify      = require('gulp-uglify'),
-    cssnano     = require('gulp-cssnano'),
-    sourcemaps  = require('gulp-sourcemaps'),
-    bourbon     = require('bourbon').includePaths,
-    neat        = require('bourbon-neat').includePaths;
+var gulp          = require('gulp'),
+    del           = require('del'),
+    sass          = require('gulp-sass'),
+    concat        = require('gulp-concat'),
+    uglify        = require('gulp-uglify'),
+    cssnano       = require('gulp-cssnano'),
+    sourcemaps    = require('gulp-sourcemaps'),
+    bourbon       = require('bourbon').includePaths,
+    neat          = require('bourbon-neat').includePaths;
 
 const static_path = './personal_portfolio/static';
 const src_path = static_path + '/src';
 const paths = {
     stylesheets: [src_path + '/stylesheets/**/*.scss'],
-    scripts: [src_path + '/scripts/**/*.js']
+    scripts: [src_path + '/scripts/**/*.js'],
+    fonts: static_path + '/fonts'
 }
+
+const fa_path = 'node_modules/font-awesome/fonts/*';
 
 gulp.task('transpile_css', function() {
     return gulp.src(paths.stylesheets)
         .pipe(sourcemaps.init())
         .pipe(sass({
-            includePaths: [paths.stylesheets].concat(bourbon, neat)
+            includePaths: [paths.stylesheets]
+              .concat(bourbon, neat)
         }).on('error', sass.logError))
         .pipe(cssnano())
         .pipe(concat('app.css'))
@@ -36,6 +40,11 @@ gulp.task('uglify', function() {
         .pipe(gulp.dest(static_path));
 })
 
+gulp.task('fonts', function() {
+  return gulp.src(fa_path)
+    .pipe(gulp.dest(paths.fonts + '/font_awesome'));
+})
+
 gulp.task('clean', function() {
     del([static_path + '/app.js', static_path + '/app.css']);
 })
@@ -43,6 +52,7 @@ gulp.task('clean', function() {
 gulp.task('watch', function() {
     gulp.watch(paths.scripts, ['uglify']);
     gulp.watch(paths.stylesheets, ['transpile_css']);
+    gulp.watch(paths.fonts, ['fonts']);
 })
 
-gulp.task('default', ['clean', 'uglify', 'transpile_css', 'watch']);
+gulp.task('default', ['clean', 'uglify', 'transpile_css', 'fonts', 'watch']);
