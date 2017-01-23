ed302d61e0504062530b393a657002fe4d4b68d3
Alan <alan.wernik@gmail.com>
2017-01-22 21:04:48 -0600

    Add mobile menu

--- gulpfile.js
+++ gulpfile.js
@@ -10,53 +10,57 @@ var gulp          = require('gulp'),
 
 const static_path = './personal_portfolio/static';
 const src_path = static_path + '/src';
-const paths = {
+const input_paths = {
     stylesheets: [src_path + '/stylesheets/**/*.scss'],
     scripts: [src_path + '/scripts/**/*.js'],
+}
+const output_paths = {
+    stylesheets: static_path + '/stylesheets',
+    scripts: static_path + '/scripts',
     fonts: static_path + '/fonts'
 }
 
 const fa_path = 'node_modules/font-awesome/fonts/*';
 
 gulp.task('transpile_css', function() {
-    return gulp.src(paths.stylesheets)
+    return gulp.src(input_paths.stylesheets)
         .pipe(sourcemaps.init())
         .pipe(sass({
-            includePaths: [paths.stylesheets]
+            includePaths: [input_paths.stylesheets]
               .concat(bourbon, neat)
         }).on('error', sass.logError))
         .pipe(cssnano())
         .pipe(concat('app.css'))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest(static_path));
+        .pipe(gulp.dest(output_paths.stylesheets));
 })
 
 gulp.task('uglify', function() {
-    return gulp.src(paths.scripts)
+    return gulp.src(input_paths.scripts)
         .pipe(sourcemaps.init())
         .pipe(uglify())
         .pipe(concat('app.js'))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest(static_path));
+        .pipe(gulp.dest(output_paths.scripts));
 })
 
 gulp.task('fonts', function() {
   return gulp.src(fa_path)
-    .pipe(gulp.dest(paths.fonts + '/font_awesome'));
+    .pipe(gulp.dest(output_paths.fonts + '/font_awesome'));
 })
 
 gulp.task('clean', function() {
     del([
-      static_path + '/app.js', 
-      static_path + '/app.css', 
-      paths.fonts + '/font_awesome'
+      output_paths.scripts + '/app.js', 
+      output_paths.stylesheets + '/app.css', 
+      output_paths.fonts + '/font_awesome'
     ]);
 })
 
 gulp.task('watch', ['fonts', 'transpile_css', 'uglify'], function() {
-    gulp.watch(paths.scripts, ['uglify']);
-    gulp.watch(paths.stylesheets, ['transpile_css']);
-    gulp.watch(paths.fonts, ['fonts']);
+    gulp.watch(input_paths.scripts, ['uglify']);
+    gulp.watch(input_paths.stylesheets, ['transpile_css']);
+    gulp.watch(input_paths.fonts, ['fonts']);
 })
 
 gulp.task('default', ['clean', 'uglify', 'transpile_css', 'fonts', 'watch']);
