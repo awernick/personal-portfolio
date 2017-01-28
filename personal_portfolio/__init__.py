51ca3c1c21f8f080edbb5dbb50014cdc43ea38b2
Alan <alan.wernik@gmail.com>
2017-01-28 01:22:45 -0600

    Change required env variable

--- personal_portfolio/__init__.py
+++ personal_portfolio/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_mail import Mail
 
 app = Flask(__name__)
-app.config.from_object(environ['APP_SETTINGS'])
+app.config.from_object(environ['PORTFOLIO_APP_SETTINGS'])
 
 mail = Mail(app)
 
