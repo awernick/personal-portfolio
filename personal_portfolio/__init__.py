1952efa6cae1cb9eaa68788a86ee00ee1c5d42e2
Alan Wernick <alan.wernik@gmail.com>
2017-04-22 16:59:16 -0500

    Extra slash

--- personal_portfolio/__init__.py
+++ personal_portfolio/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint, Flask, current_app as app
 
 port_app = Blueprint('portfolio', __name__,
                 template_folder='templates', static_folder='static',
-                static_url_path='/static/')
+                static_url_path='/static')
 
 from . import views
 
