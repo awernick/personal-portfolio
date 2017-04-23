4c300a54724df871f2918e8c8cf33a2c894df473
Alan Wernick <alan.wernik@gmail.com>
2017-04-22 23:32:46 -0500

    Add speechwtext for deployment

--- personal_portfolio/__init__.py
+++ personal_portfolio/__init__.py
@@ -1,8 +1,7 @@
 from flask import Blueprint, Flask, current_app as app
 
 port_app = Blueprint('portfolio', __name__,
-                template_folder='templates', static_folder='static',
-                static_url_path='/static')
+                template_folder='./templates', static_folder='./static/portfolio')
 
 from . import views
 
--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,31 @@
 from personal_portfolio import app, port_app
-from flask import Flask
+from speech2text import speech_app
+from flask import Flask, url_for, jsonify
 from flask_mail import Mail
 from os import environ
 
 app = Flask(__name__)
 app.config.from_object(environ['PORTFOLIO_APP_SETTINGS'])
+mail = Mail(app)
 
 app.register_blueprint(port_app)
-mail = Mail(app)
+app.register_blueprint(speech_app, url_prefix='/speech')
+
+def has_no_empty_params(rule):
+    defaults = rule.defaults if rule.defaults is not None else ()
+    arguments = rule.arguments if rule.arguments is not None else ()
+    return len(defaults) >= len(arguments)
+
+@app.route("/site-map")
+def site_map():
+    links = []
+    for rule in app.url_map.iter_rules():
+        # Filter out rules we can't navigate to in a browser
+        # and rules that require parameters
+        if "GET" in rule.methods and has_no_empty_params(rule):
+            url = url_for(rule.endpoint, **(rule.defaults or {}))
+            links.append((url, rule.endpoint))
+    return jsonify(links)
 
 if __name__ == "__main__":
     app.run()
